47ecf905cf1361f746ec0add47408aa5e30c8d45
Yagni Patel <140079410+yagni711@users.noreply.github.com>
2023-07-29 17:55:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Positions</title>
+<link rel="icon" type="image/x-icon" href="img/logo.png">
+<!-- <link rel="stylesheet" href="styles.css"> -->
+<style type="text/css">
+  body{
+height:1600px;
+/*background-color: red;*/
+}
+.car{
+  width:600px;
+  height: 400px;
+  margin-left:400px;
+  position: relative;
+  left:30px;
+  top:30px;
+}
+.jerry{
+  width:70px;
+  height:70px;
+  margin-left:-52px;
+  position: fixed;
+  top:90px;
+}
+.dog{
+  width:150px;
+  height:150px;
+  margin-left:-330px;
+  position: fixed;
+  top:30px;
+}
+.tom{
+  width:200px;
+  height:200px;
+  margin-left:70px;
+  position: fixed;
+  top:120px;
+}
+.train{
+  width:600px;
+  height: 400px;
+  margin-left:460px;
+  position: relative;
+  top:540px;
+}
+</style>
+</head>
+<body>
+  <img src="img/car4.png" alt="car" title="car" class="car"/>
+  <img src="img/jerry.png" alt="jerry" title="jerry" class="jerry"/>
+   <img src="img/tom.png" alt="tom" title="tom" class="tom"/>
+   <img src="img/dog1.png" alt="dog" title="dog" class="dog"/>
+   <img src="img/train1.png" alt="train" title="train" class="train"/>
+</body>
+</html>
+
